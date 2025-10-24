855bba70a9c6f8e98008346e89c4dc7f7914d5a9
sonikaur21 <skaur1902@gmail.com>
2025-10-24 14:19:37 -0400

    added apikey

--- src/Dictonary.js
+++ src/Dictonary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import "./Dictonary.css";
+import axios from "axios";
+
 export default function Dictonary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+    let apiKey = "79e2b848de54da3deo0aafeff8t7fa08";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
